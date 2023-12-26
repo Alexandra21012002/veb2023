8c45255e5dff5284ae70ce19ba8dadfa2c2d7eab
Maksim Khrestinin <maksim.khrestinin@gmail.com>
2023-12-26 23:46:25 +0400

    Errors fixed

--- task6/src/components/movie-details/movie-details.jsx
+++ task6/src/components/movie-details/movie-details.jsx
@@ -1,11 +1,58 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import { EditIcon } from "../../assets/icons/edit-icon";
+import { StarIcon } from "../../assets/icons/star";
 import "./movie-details.css";
 
 const MovieDetails = ({
   movie,
   callEditMovie
 }) => {
+  const [isFavorite, setIsFavorite] = useState(movie.isFavorite);
+
+  useEffect(() => {
+    setIsFavorite(movie.isFavorite);
+  }, [movie.isFavorite]);
+
+  const toggleFavorite = () => {
+    setIsFavorite(!isFavorite);
+    updateFavoritesInDatabase(movie.id, !isFavorite);
+    // getAll()
+  };
+
+  const updateFavoritesInDatabase = async (movieId, isFavorite) => {
+    try {
+      const response = await fetch(`http://localhost:3300/movies/${movieId}`, {
+        method: 'PATCH',
+        headers: {
+          'Content-Type': 'application/json',
+        },
+        body: JSON.stringify({ isFavorite }),
+      });
+
+      if (!response.ok) {
+        throw new Error('Ответ сети пришел с ошибкой');
+      }
+
+      window.location.reload();
+    } catch (error) {
+      console.error('Ошибка обновления избранного:', error);
+    }
+  };
+
+  // function getAll() {
+  //   return new Promise((resolve, reject) => {
+  //     setTimeout(() => {
+  //       fetch("http://localhost:3300/movies")
+  //         .then((res) => res.json())
+  //         .then((data) => resolve(data))
+  //         .catch(() => {
+  //           alert("Error in fetch!");
+  //           reject("Error in fetch!");
+  //         });
+  //     }, 300); // 300 миллисекунд (0.3 секунды) задержки
+  //   });
+  // }
+
   return (
     <div className="movie-details">
       <div
@@ -34,7 +81,11 @@ const MovieDetails = ({
           alt="img of film"
         ></img>
         <div className="info">
-          <p className="title-of-film">{movie.title}</p>
+          <p className="title-of-film">{movie.title}{" "}
+            <span onClick={toggleFavorite}>
+              <StarIcon filled={isFavorite} />
+            </span>
+          </p>
           <p className="param" style={{ marginTop: 0 }}>
             {movie.director}
           </p>
